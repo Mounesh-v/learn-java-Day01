35cd777ad9d769708b54b44960e9d3fb5ee4a62d
Mounesh v <mouneshv696@gmail.com>
2025-08-16 18:36:43 +0530

    learning Java

---
+++ Day1/Args.java
@@ -0,0 +1,6 @@
+public class Args{
+    public static void main(String[] args) {//args => array
+        System.out.println(args[0]);//1,2,3 5=>1,2,3
+        System.out.println(args[1]);//1,2,3 5=>5    
+    }
+}
---
+++ Day1/Hello.java
@@ -0,0 +1,6 @@
+public class Hello {//Hello=>class
+    public static void main(String[] args) {
+        System.out.print("Hello World");// out=>output
+        System.out.println("Hello World");//ln=> new line
+    }
+}
