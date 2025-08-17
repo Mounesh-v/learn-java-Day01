b8978f069db6c48fa9c25b5f40caeb6536c67165
Mounesh v <mouneshv696@gmail.com>
2025-08-17 21:08:31 +0530

    Added Day 2 code (datatypes.java) and updated README

---
+++ Day2/datatypes.java
@@ -0,0 +1,18 @@
+package Day2;
+
+public class datatypes {
+    public static void main(String[] args) {
+        // datatype variable_Name = value;
+        // integers
+        int var1 = 25;
+        // var = 25; ---> ERROR
+        // var = 30;   ---> it will update var value
+        System.out.println(var1);
+        //float
+        float var2 = 20.23f;
+        System.out.println(var2);
+        // string
+        String string = "Hello World";
+        System.out.println(string);
+    }
+}
