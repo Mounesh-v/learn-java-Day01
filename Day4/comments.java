44152e50d6ae362d3e3ef779e57404ca145fa4f3
Mounesh v <mouneshv696@gmail.com>
2025-08-19 14:00:21 +0530

    Day4 Comments And Naming Conventions in Java

---
+++ Day4/comments.java
@@ -0,0 +1,19 @@
+package Day4;
+
+public class comments {
+    public static void main(String[] args) {
+        // Keywords(Specific Reserved Keywords) => like int,float,static,etc....
+        // This is a Single Line Comments
+        /*
+          this is a multiline Comments
+         */
+        /*
+         * this is the Documented Comments
+         */
+        /*Uses of Comments
+        1.Readabilty
+        2.Testing
+        3.Debugging
+         */
+    }
+}
